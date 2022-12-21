3889a286316589f8681d1a9adddcac6e33a2b340
akshkumar0202@gmail.com <akshkumar0202@gmail.com>
2022-12-21 22:15:57 +0530

    minorPush

---
+++ src/Miniprj/swappppp.java
@@ -0,0 +1,22 @@
+package Miniprj;
+
+public class swappppp {
+
+	public static void main(String[] args) {
+		// write a program to sawp numbers
+		  int a= 5;
+		  int b =6;
+		   
+		  System.out.println("  numbers before swaping are  " + a +" & " + b);
+		  
+		  int temp;
+		  
+		   temp = a;
+		   a = b;
+		   b= temp;
+		   System.out.println(" enter the number  "  + "  "+  a);
+		   System.out.println("  enter the 2nd number " +"  "+ b);
+
+	}
+
+}
